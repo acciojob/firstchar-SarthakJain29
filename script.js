b48898456a2d1e7a075fd7e1a5af846f8166aea8
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-06-05 15:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function firstChar(text) {
-   let res = " ";	
+	
    for(let i=0; i<text.length; i++){
-	   if(text[i] !== " ") res = text[i];
-	   break;
+	   if(text[i] !== " ") {
+		   return text[i];
+	   }
    }
-   return res
+   return "";
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// //Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
